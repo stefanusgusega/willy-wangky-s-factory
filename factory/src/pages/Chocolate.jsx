24210a923ccd82a580d3ba1e0fc0e27fbb84cd5b
stefanusgusega <gunawanstefanus006@gmail.com>
2020-11-27 23:20:27 +0700

    modify gitignore and implement Chocolate.jsx

--- factory/src/pages/Chocolate.jsx
+++ factory/src/pages/Chocolate.jsx
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import NavigationBar from '../components/NavigationBar';
 
 class Chocolate extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            chocos: []
+        };
+    }
     state = {  }
     render() { 
         return ( 
